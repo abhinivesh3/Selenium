f26c01f49e89199f382c2ae9f75132d62c995ee6
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-10 11:26:44 +0530

    Added alert functionlity

---
+++ src/main/java/org/example/Alerts.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Alerts {
+    public static  void  main(String[] args){
+        System.setProperty("webdriver.chrome.driver","/Users/abhi-21739/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("");
+
+        WebElement alertBox=driver.findElement(By.xpath(""));
+        alertBox.click();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+
+        WebElement confirmButton = driver.findElement(By.xpath(""));
+        confirmButton.click();
+        Alert confirmAlert = driver.switchTo().alert();
+        alert.dismiss();
+
+        WebElement promptBox = driver.findElement(By.xpath(""));
+        promptBox.click();
+        Alert promptAlert=driver.switchTo().alert();
+        alert.sendKeys("Hey!!!");
+        alert.accept();
+    }
+}
