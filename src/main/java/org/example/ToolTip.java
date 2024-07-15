648c6ce4d1433dea126af278a2aeab51a0dcf264
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-15 14:34:19 +0530

    Added ToolTip program

--- src/main/java/org/example/ToolTip.java
+++ src/main/java/org/example/ToolTip.java
@@ -1,2 +1,25 @@
-package org.example;public class ToolTip {
+package org.example;
+
+import jdk.nashorn.internal.runtime.regexp.joni.constants.internal.OPCode;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ToolTip {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/abhi-21739/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+
+        driver.get("https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_tooltip");
+
+        WebElement name=driver.findElement(By.xpath("/html/body/div"));
+        Thread.sleep(3000);
+        String toolTripText = name.getAttribute("tooltiptext");
+        System.out.println(toolTripText);
+
+    }
 }
