4e9760760bc238fc24eec999076484f00f8c6825
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-15 14:40:49 +0530

    Added Drag and Drop program

--- src/main/java/org/example/DragAndDrop.java
+++ src/main/java/org/example/DragAndDrop.java
@@ -1,2 +1,25 @@
-package org.example;public class DragAndDrop {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDrop {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+
+        driver.get("");
+        WebElement Frome =driver.findElement(By.id(""));
+        WebElement To=driver.findElement(By.id(""));
+
+        Actions actions=new Actions(driver);
+//        actions.clickAndHold(Frome).moveToElement(To).release(To).build().perform();
+        actions.dragAndDrop(Frome,To).build().perform();
+    }
 }
