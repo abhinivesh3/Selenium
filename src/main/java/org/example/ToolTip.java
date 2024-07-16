d77a47223bbb7a80f3c28d8c2fd56ff756cf0298
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-16 14:27:03 +0530

    Added Tooltip program

--- src/main/java/org/example/ToolTip.java
+++ src/main/java/org/example/ToolTip.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import jdk.nashorn.internal.runtime.regexp.joni.constants.internal.OPCode;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
