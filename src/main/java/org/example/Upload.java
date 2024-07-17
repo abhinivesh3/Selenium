935bd512b2eee3a6d3656fd8caf0df900be46ed8
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-17 17:57:12 +0530

    Added Upload functionality

---
+++ src/main/java/org/example/Upload.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+public class Upload {
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/abhi-21739/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://tiiny.host/?content=pdf");
+
+        WebElement uploadedButtom = driver.findElement(By.xpath("//*[@id=\"content-selector-tabpane-pdf\"]/div/div/div[2]/button"));
+        uploadedButtom.click();
+
+        Thread.sleep(3000);
+        StringSelection selection = new StringSelection("/Users/abhi-21739/downloads/11MB.pdf");
+
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);
+
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        Thread.sleep(3000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+    }
+}
