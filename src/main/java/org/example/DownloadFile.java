2986b3a69ed6e405e1a1ba56d532387c07dff362
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-17 17:58:38 +0530

    Added download file functionality

--- src/main/java/org/example/DownloadFile.java
+++ src/main/java/org/example/DownloadFile.java
@@ -1,2 +1,35 @@
-package org.example;public class DownloadFile {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+
+public class DownloadFile {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","/Users/abhi-21739/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://file-examples.com/index.php/sample-documents-download/sample-xls-download/#google_vignette");
+
+        WebElement downloadLink = driver.findElement(By.linkText("DOWNLOAD SAMPLE XLS FILE"));
+       // WebElement downloadLink = driver.findElement(By.xpath("//*[@id='table-files']/tbody/tr[1]/td[5]/a"));
+        downloadLink.click();
+
+        Thread.sleep(3000);
+
+        File fileLocation = new File("/Users/abhi-21739/downloads");
+        File[] totalFiles = fileLocation.listFiles();
+
+        for(File file:totalFiles){
+            if(file.getName().equals("file_example_XLS_10.xls")){
+                System.out.println("File is download");
+                break;
+            }
+        }
+    }
 }
