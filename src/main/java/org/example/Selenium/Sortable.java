9bb4dbc8ccab40d619b08b71d9903d0c2062515c
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-22 11:46:50 +0530

    initial setup for testNG

--- src/main/java/org/example/Selenium/Sortable.java
+++ src/main/java/org/example/Selenium/Sortable.java
@@ -1,6 +1,5 @@
 package org.example.Selenium;
 
-import org.bouncycastle.jcajce.provider.drbg.DRBG;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
