4a972ed1ecb5c1c71673339851d0244df40277af
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-22 10:59:56 +0530

    Added Calendar functionality

--- src/main/java/org/example/Calendar.java
+++ src/main/java/org/example/Calendar.java
@@ -1,2 +1,27 @@
-package org.example;public class Calendar {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Calendar {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.navigate().to("https://www.calendar-365.com/calendar/2024/July.html");
+
+        WebElement calendar = driver.findElement(By.id(""));
+        calendar.sendKeys("01/07/2024"+ Keys.ENTER);
+
+        WebElement nextButton = driver.findElement(By.xpath(""));
+        nextButton.click();
+
+        WebElement deleteToBeGiven = driver.findElement(By.xpath(""));
+        deleteToBeGiven.click();
+    }
 }
