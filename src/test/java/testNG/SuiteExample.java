f862f7fb5ce6c91c7129c498a50e3f6bf11219d5
Abhi Nivesh R <abhinivesh382002@gmail.com>
2024-07-24 21:29:38 +0530

    Added testNG Suite functionality

--- src/test/java/testNG/SuiteExample.java
+++ src/test/java/testNG/SuiteExample.java
@@ -1,2 +1,47 @@
-package testNG;public class SuiteExample {
+package testNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class SuiteExample {
+
+    WebDriver driver;
+    long startTime, endTime;
+
+    @BeforeSuite
+    public void launchBrowser(){
+        startTime = System.currentTimeMillis();
+
+        System.setProperty("webdriver.chrome.driver","/Users/abhi-21739/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void openGoogle(){
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void openBing(){
+        driver.get("https://www.bing.com");
+    }
+
+    @Test
+    public void openYahoo(){
+        driver.get("https://www.yahoo.com");
+    }
+
+    @AfterSuite
+    public void closeBrowser(){
+        driver.quit();
+        endTime = System.currentTimeMillis();
+        long totalTime = endTime-startTime;
+        System.out.println("Total time taken "+totalTime);
+    }
 }
